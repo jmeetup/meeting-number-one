1b5111b8ea6be71012566edc300c9dde908b7445
Олег Буршинов <mrzoom.214@gmail.com>
2018-11-13 23:30:57 +0300

    JaCoCo enum example

---
+++ java-bytecode/src/main/java/com/github/mrzo0m/Hobbits.java
@@ -0,0 +1,26 @@
+package com.github.mrzo0m;
+
+public enum Hobbits {
+
+    BILBA_LABINGI,
+
+    BILBO_BAGGINS,
+
+    BILBO_GARDNER,
+
+    BILL_BUTCHER,
+
+    BINGO_BAGGINS,
+
+    BLANCO,
+
+    BLANCO_BRACEGIRDLE,
+
+    BOB,
+
+    BODO_PROUDFOOT,
+
+    FRODO_BAGGINS,
+
+    FRODO_GARDNER
+}
